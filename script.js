160d94b3267f1b6d0df7a2d414bd71250d19e7d3
husttroy <troyathust@gmail.com>
2017-06-18 18:18:02 -0700

    fix the pagination issue in the popover dialog.

--- script.js
+++ script.js
@@ -11,10 +11,8 @@ function doSearch(text) {
 
         while (window.find(text)) {
 			//create link to dialog box
-			//document.execCommand("createLink", false, "https://www.facebook.com/");
-			document.execCommand('insertHTML', false, '<a href="#" runat="server" id="dialogLink">' + text + '</a>')
-			
-            //sel.collapseToEnd();
+			//document.execCommand('insertHTML', false, '<a href="#" runat="server" id="dialogLink">' + text + '</a>');
+			document.execCommand('insertHTML', false, '<a data-toggle="popover" id="popoverLink" data-title="Missing Exception Handling" data-container="body" data-html="true">' + text + '</a>');
         }
 		
 		// get the selection back because inserting HTML closed it
@@ -39,7 +37,67 @@ function doSearch(text) {
 var myText = "out.write(myByteBuffer);";
 doSearch(myText);
 
-$("#dialogLink").click( function() {
-	$("#dialog").dialog("open");
-	return false;
+
+var codeStringOne = '<span style="background-color: #FFFF00">try {</span> \n\r out.write(myByteBuffer); \n\r<span style="background-color: #FFFF00">} catch (IOException e) { \n\rthrow new IllegalStateException(e); \n\r}</span>';
+var codeStringTwo = '<span style="background-color: #FFFF00">try {</span> \n\r out.write(myByteBuffer); \n\r<span style="background-color: #FFFF00">} catch (ClosedChannelException e) { \n\rSystem.exit(1); \n\r}</span>';
+
+// add content to the popover
+$("[data-toggle=popover]").popover({
+content: '<div class="pagination-container"><div data-page="1"><p>This code could be improved with exception handling:</p><pre><code>'+ codeStringOne +'</code></pre>'
++ '<p>This pattern is used by 3370 snippets in 578 GitHub repositories</p>'
++ '<a href="https://www.google.com/">See this in a GitHub example</a></div>'
++ '<div data-page="2" style="display:none;"><p>This code could be improved with exception handling:</p><pre><code>'+ codeStringTwo +'</code></pre>'
++ '<p>This pattern is used by 3370 snippets in 578 GitHub repositories</p>'
++ '<a href="https://www.google.com/">See this in a GitHub example</a></div>'
++ '<div data-page="3" style="display:none;"><p>This code could be improved with exception handling:</p><pre><code>'+ codeStringOne +'</code></pre>'
++ '<p>This pattern is used by 3370 snippets in 578 GitHub repositories</p>'
++ '<a href="https://www.google.com/">See this in a GitHub example</a></div>'
++ '<div class="pagination pagination-centered">'
++'<ul class="page_control"><li class="active" data-page="1"><a href="#" >1</a></li>'
++ '<li data-page="2"><a href="#" >2</a></li>'
++ '<li data-page="3"><a href="#" >3</a></li>'
++ '</ul></div>'
++ '</div>'
+});
+
+// script for running the pagination
+$(document.body).on('shown.bs.popover', function () {
+  console.log("shown");
+  var paginationHandler = function(){
+    // store pagination container so we only select it once
+    var $paginationContainer = $(".pagination-container"),
+        $pagination = $paginationContainer.find('.pagination ul');
+
+    console.log($paginationContainer);
+    console.log($pagination);
+
+    // click event
+    $pagination.find("li a").on('click.pageChange',function(e){
+        e.preventDefault();
+
+        // get parent li's data-page attribute and current page
+        var parentLiPage = $(this).parent('li').data("page"),
+            currentPage = parseInt( $(".pagination-container div[data-page]:visible").data('page') ),
+            numPages = $paginationContainer.find("div[data-page]").length;
+        
+        // make sure they aren't clicking the current page
+        if ( parseInt(parentLiPage) !== parseInt(currentPage) ) {
+            // hide the current page
+            $paginationContainer.find("div[data-page]:visible").hide();
+
+            if ( parentLiPage === '+' ) {
+                // next page
+                $paginationContainer.find("div[data-page="+( currentPage+1>numPages ? numPages : currentPage+1 )+"]").show();
+            } else if ( parentLiPage === '-' ) {
+                // previous page
+                $paginationContainer.find("div[data-page="+( currentPage-1<1 ? 1 : currentPage-1 )+"]").show();
+            } else {
+                // specific page
+                $paginationContainer.find("div[data-page="+parseInt(parentLiPage)+"]").show();
+            }
+
+        }
+    });
+  };
+  $( document ).ready( paginationHandler );
 });
