8122fd1a5596d357701369da80a7497d773cc4cf
Anastasia Reinhardt <areinhardt14@georgefox.edu>
2017-07-12 14:55:16 -0700

    Removed redundant violations and enhanced violation messages

--- server/src/edu/ucla/cs/model/Violation.java
+++ server/src/edu/ucla/cs/model/Violation.java
@@ -15,28 +15,58 @@ public class Violation {
 	    switch (type) {
 	        case IncorrectPrecondition:
 	            message = "You may want to check whether a condition is true before calling "
-	                    + item + ". " + vioPattern.support + " GitHub code examples also do this.\n"
+	                    + ((APICall) item).getName() + "(). " + vioPattern.support 
+	                    + " GitHub code examples also do this.\n"
 	                    + vioPattern.description;
 	            break;
 	        case MissingMethodCall:
-	            message = "You may want to call " + item + " here." + vioPattern.support +
+	            message = "You may want to call " + ((APICall) item).getName() + "() here. " + vioPattern.support +
 	                " Github code examples also do this.\n" + vioPattern.description;
 	            break;
 	        case MissingStructure:
-	            message = "You may want to call a control structure here. " + vioPattern.support +
-	                " Github code examples also do this.\n" + vioPattern.description;
+	            message = "You may want to use " + getControlStructureType() + " here. " 
+	                    + vioPattern.support + " Github code examples also do this.\n" 
+	                    + vioPattern.description;
 	            break;
 	        case DisorderMethodCall:
-    	        message = "You may want to call " + item + " in a different order. "
+    	        message = "You may want to call " + ((APICall) item).getName() + "() in a different order. "
         	        + vioPattern.support + "GitHub code examples also do this.\n"
         	        + vioPattern.description;
                 break;
 	        case DisorderStructure:
-	            message = "You may want to call " + item + " in a different order. "
+	            message = "You may want to call " + getControlStructureType() + " in a different order. "
 	                + vioPattern.support + "GitHub code examples also do this.\n"
 	                + vioPattern.description;
 	            break;
 	    }
 	    return message;
+	}
+	
+	private String getControlStructureType() {
+	    String structure;
+        if (item == ControlConstruct.TRY || item == ControlConstruct.CATCH) {
+            structure = "a try-catch block";
+        }
+        else if (item == ControlConstruct.FINALLY) {
+            //TODO: is it necessary to separate this from try-catch?
+            structure = "a try-catch-finally block";
+        }
+        else if (item == ControlConstruct.IF) {
+            structure = "an if-statement";
+        }
+        // I would like to use an else violation for if-else clauses
+        // to keep them distinct from solo if-statements
+        else if (item == ControlConstruct.ELSE) {
+            structure = "an if-else statement";
+        }
+        else if (item == ControlConstruct.LOOP) {
+            structure = "a loop";
+        }
+        else {
+            // I'm not sure what the default should be.
+            structure = "a control structure";
+        }
+        
+        return structure;
 	}
 }
