6c9d542fa97eaeeff0d834463b9cb084d5770c23
husttroy <troyathust@gmail.com>
2018-06-20 13:40:36 -0700

    some edits

--- chrome/script.js
+++ chrome/script.js
@@ -6,7 +6,8 @@ function getRepoFromURL(url){
 
 
 // setup the websocket
-var socket = new WebSocket('ws://131.179.224.239:4000/');
+//var socket = new WebSocket('ws://131.179.224.239:4000/');
+var socket = new WebSocket('ws://127.0.0.1:4000/');
 
 // Handle any errors that occur.
 socket.onerror = function(error) {
